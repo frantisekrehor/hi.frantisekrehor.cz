6997c3b20bf76c250bb21e2c33ed9ccda4225add
Frantisek Rehor <hi@frantisekrehor.cz>
2017-06-30 20:37:30 +0200

    initial

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html class="no-js" lang=""><head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MPM7DPF');</script>
+        <!-- End Google Tag Manager -->
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="František Řehoř -- Data engineer -- Budweis (CZ)">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Facebook Open Graph meta -->
+        <meta property="og:title" content="František Řehoř">
+        <meta property="og:description" content="František Řehoř -- Data engineer -- Budweis (CZ)">
+        <meta property="og:image" content="http://frantisekrehor.cz/fr.jpg">
+        <meta property="og:url" content="http://frantisekrehor.cz">
+
+        <link rel="shortcut icon" href="favicon.png">
+
+        <title>František Řehoř</title>
+
+        <!-- Included Font Files -->
+        <link href="css" rel="stylesheet" type="text/css">
+        <!-- Included CSS Files -->
+        <link rel="stylesheet" type="text/css" href="app.min.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+    </head>
+    <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPM7DPF"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        
+        <section class="main-container">
+            <div class="blocks">
+                <div class="block block--left" style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                    <div class="block__inner">
+                        <div class="block-content">
+                            <h1 class="block__title">
+                                <span style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">Hey! I'm Frank,</span>
+                            </h1>
+                            <h1 class="block__title">
+                                <span style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">an aspiring <em>data engineer</em> living in&nbsp;<a href="https://www.google.cz/maps/place/Ceske+Budejovice/" target="_blank" style="text-decoration: underline;"><em>Budweis</em></a> working for <a href="http://keboola.asia/" target="_blank" style="text-decoration: underline;"><em>Keboola Asia</em></a></span>
+                            </h1>
+                            <div class="block__cta" style="overflow: visible;">
+                                <a href="mailto:hi@frantisekrehor.cz" style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
+                                    <span>Get in touch</span>
+                                </a>
+                            </div>
+                            <div class="block__social">
+                                <a class="block__social__link" target="_blank" href="https://linkedin.com/in/frantisekrehor">
+                                    <svg><use xlink:href="svg-defs.svg#linkedin"/></svg>
+                                </a>
+                                <a class="block__social__link" target="_blank" href="http://twitter.com/frantisekrehor">
+                                    <svg><use xlink:href="svg-defs.svg#twitter"/></svg>
+                                </a>
+                                <a class="block__social__link" target="_blank" href="http://github.com/frantisekrehor">
+                                    <svg><use xlink:href="svg-defs.svg#github"/></svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block__bgi" style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;"></div>
+                </div>
+                <div class="block block--right">
+                    <div class="block__inner">
+                        <div class="content content--about" style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
+                            <div class="content__tag-wrapper">
+                                <h2 class="content__tag">Who</h2>
+                            </div>
+                            <div class="content__inner">
+                                <p>Hey what's up?<br /> My full name's František Řehoř, but nobody beyond the 
+                                area of Czech&nbsp;Republic is able to pronounce it, thus simply Frank. I'm an aspiring data engineer living in the <a href="https://www.google.cz/maps/place/Ceske+Budejovice/" target="_blank">city of Budweiser beer</a> and currently working for Bali-based <a href="http://keboola.asia/" target="_blank">Keboola Asia</a>.</p>
+                                
+                            </div>
+                        </div>
+                        <div class="content content--about" style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
+                            <div class="content__tag-wrapper">
+                                <h2 class="content__tag">What</h2>
+                            </div>
+                            <div class="content__inner">
+                                <p>I'm fluent in <em>SQL</em> <em>many BI tools</em>
+                                and constantly improving my <em>Python</em> programming skills. 
+                                I know <em>HTML</em> <em>CSS</em> and a bit of <em>PHP</em>.
+                                </p>
+                                <p>
+                                Experiencing with <em>REST APIs</em> and wrangling <em>data of any format</em> and <em>any source</em> on daily basis. <em>Command line</em> is becoming my good friend.</p>
+                                
+                                <p>I have a 5-year experience in <em>online marketing</em> and <em>ecommerce</em>.</p>
+                                <p><em></em>Resolutions for 2017: Expand my knowledge of statistics and learn more about data mining and machine learning techniques.</p>
+                                
+                            </div>
+                        </div>
+                        <div class="content content--about" style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
+                            <div class="content__tag-wrapper">
+                                <h2 class="content__tag">Where</h2>
+                            </div>
+                            <div class="content__inner">
+                                <p>Wanna meet and talk?<br />Stop by in my favourite <a href="http://www.coworkingcb.cz/" target="_blank">coworking space</a> in the center of Budweis or simply drop a line.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Initialize JS Plugins/Shims -->
+        <script src="jquery.js"></script>
+        <script src="CSSRulePlugin.min.js"></script>
+        <script src="TweenMax.min.js"></script>
+        <script src="app.min.js"></script>
+
+    
+
+</body></html>
