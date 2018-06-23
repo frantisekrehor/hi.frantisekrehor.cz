840622e37d71f96174ba129328121150386540f7
Frantisek Rehor <hi@frantisekrehor.cz>
2018-06-23 02:06:08 +0200

    adding and changing some texts

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">        
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="František Řehoř -- BI Architect &amp; Developer">
+        <meta name="description" content="František Řehoř | BI &amp; ETL Developer">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="keywords" content="František Řehoř, Business Intelligence, data, ETL, developer">
         <meta name="author" content="František Řehoř">
 
         <!-- Facebook Open Graph meta -->
         <meta property="og:title" content="František Řehoř">
-        <meta property="og:description" content="František Řehoř -- BI Architect &amp; Developer">
+        <meta property="og:description" content="František Řehoř | BI &amp; ETL Developer">
         <meta property="og:image" content="http://hi.frantisekrehor.cz/frantisek_rehor.jpg">
         <meta property="og:url" content="http://hi.frantisekrehor.cz">
 
@@ -25,14 +25,14 @@
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:domain" content="http://hi.frantisekrehor.cz">
         <meta name="twitter:title" content="František Řehoř">
-        <meta name="twitter:description" content="BI Architect &amp; Developer">
+        <meta name="twitter:description" content="BI &amp; ETL Developer">
         <meta name="twitter:image:src" content="http://hi.frantisekrehor.cz/frantisek_rehor.jpg">
 
         <meta itemprop="image" content="http://hi.frantisekrehor.cz/frantisek_rehor.jpg">
 
         <link rel="shortcut icon" href="favicon.png">
 
-        <title>František Řehoř -- BI Architect &amp; Developer</title>
+        <title>František Řehoř | BI &amp; ETL Developer</title>
 
         <!-- Included Font Files -->
         <link href="css" rel="stylesheet" type="text/css">
@@ -51,10 +51,10 @@
                     <div class="block__inner">
                         <div class="block-content">
                             <h1 class="block__title">
-                                <span style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);"><b>Hey! I'm Frank</b></span>
+                                <span style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);"><b>Hi there, I'm Frank</b></span>
                             </h1>
                             <h1 class="block__title">
-                                <span style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">BI Architect &amp; Developer living in&nbsp;<a href="https://www.google.cz/maps/place/Ceske+Budejovice/" target="_blank" style="text-decoration: underline;color: white;">Budweis</a> working for <a href="http://keboola.asia/" target="_blank" style="text-decoration: underline;color: white;">Keboola Asia</a></span>
+                                <span style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">BI &amp; ETL Developer<br />living in&nbsp;<a href="https://www.google.cz/maps/place/Ceske+Budejovice/" target="_blank" style="text-decoration: underline;color: white;">České Budějovice</a></span>
                             </h1>
                             <div class="block__cta" style="overflow: visible;">
                                 <a href="mailto:hi@frantisekrehor.cz" style="transform: matrix(1, 0, 0, 1, 0, 0); visibility: inherit; opacity: 1;">
@@ -83,13 +83,11 @@
                                 <h2 class="content__tag">Who</h2>
                             </div>
                             <div class="content__inner">
-                                <p>Hey what's up?<br /> My full name's František Řehoř, but nobody beyond the 
-                                Czech&nbsp;Republic area is able to pronounce it, thus simply Frank. 
-                                </p>
-                                <p>
-                                I'm a BI developer living in the <a href="https://www.google.cz/maps/place/Ceske+Budejovice/" target="_blank">city of Budweiser beer</a> and currently working mostly for Bali-based <a href="http://keboola.asia/" target="_blank">Keboola Asia</a>.
-                                </p>
-                                
+                                <p>Hi there!</p>
+                                <p> My full name is František Řehoř... Don't worry, nobody beyond the 
+                                Czech&nbsp;Republic is able to pronounce it, thus call me simply Frank :-) 
+                                I'm a BI &amp; ETL developer living in the <a href="https://www.google.cz/maps/place/Ceske+Budejovice/" target="_blank">city of Budweiser beer</a>.
+                                </p>   
                             </div>
                         </div>
                         <div class="content content--about" style="visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
@@ -97,14 +95,13 @@
                                 <h2 class="content__tag">What</h2>
                             </div>
                             <div class="content__inner">
-                                <p>I'm fluent in <em>SQL</em> and many BI tools like <em>Power BI</em>.
-                                Constantly improving my <em>Python</em> programming skills. 
-                                I know <em>HTML</em> <em>CSS</em> and a bit of <em>PHP</em>.
+                                <p>I'm fluent in <em>SQL</em> and reporting tools like <em>Power BI</em>.
+                                I'm constantly improving my programming skills, mostly in <em>Python</em>. <em>Command line</em>, <em>Docker</em> and <em>GIT</em> are slowly becoming my good friends.</p>
                                 </p>
                                 <p>
-                                I'm designing <em>ETL procedures</em>, experiencing with <em>REST APIs</em> and wrangling <em>data</em> of any format and source on daily basis. <em>Command line</em> is slowly becoming my good friend.</p>
+                                For my clients I design and build <em>reports</em> and <em>ETL procedures</em> with <em><a href="https://www.keboola.com/" target="_blank">Keboola Connection</a></em>. On daily basis, I'm experiencing with <em>REST API</em>s and wrangling data of any format and source. 
                                 
-                                <p>I have a 5+ years experience in <em>online marketing</em> <em>ecommerce</em> and <em>web analytics</em>. Previously, I worked for clients from <em>telco</em> and <em>online payments</em> industry.</p>
+                                <p>I have a 5+ years experience in <em>online marketing</em>, <em>ecommerce</em>, <em>web analytics</em>, and with retail and wholesale. I have cooperated on projects for clients, i.e. from <em>telco</em> or <em>banking</em> industry.</p>
 
                                 
                             </div>
@@ -116,10 +113,12 @@
                             <div class="content__inner">
                                 <p>
                                     Wanna meet and talk?<br /> 
-                                    Drop me a line or stop by in <a href="https://www.cowocb.cz/" target="_blank">cowocb</a> coworking space in the center of Budweis.
+                                    Drop me a line or stop by in <a href="https://www.cowocb.cz/" target="_blank">cowocb</a> in the center of České Budějovice.
                                 </p>
                                 <p>
-                                hi@frantisekrehor.cz
+                                <a href="mailto:hi@frantisekrehor.cz">hi@frantisekrehor.cz</a>, 
+                                <a href="https://linkedin.com/in/frantisekrehor" target="_blank">LinkedIn</a>, 
+                                <a href="https://twitter.com/frantisekrehor" target="_blank">Twitter</a>
                                 </p>
                             </div>
                         </div>
